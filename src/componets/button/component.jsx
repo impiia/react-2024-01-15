fd868d9dabf720f1b759b0d39471f72428954a4e
impiia <impiia28@gmail.com>
2024-01-29 12:12:07 +0400

    Added counters (+/-) for restaurants and implemented buttons for each restaurant

---
+++ src/componets/button/component.jsx
@@ -0,0 +1,10 @@
+import style from './styles.module.scss';
+import classNames from "classnames";
+
+export const Button = ({ onClick, children, className, disabled }) => {
+    return (
+        <button className={classNames(style.root, className)} onClick={onClick} disabled={disabled}>
+            {children}
+        </button>
+    )
+}
--- src/componets/restaurants/component.jsx
+++ src/componets/restaurants/component.jsx
@@ -1,11 +1,24 @@
-import { Restaurant } from "../restaurant/component"
+import { useState } from 'react';
+import { Button } from '../button/component';
+import { Restaurant } from '../restaurant/component';
+import style from './styles.module.scss';
 
 export const Restaurants = ({ restaurants }) => {
+    const [selectedRestaurant, setSelectedRestaurant] = useState(restaurants[0] || null);
+
+    const selectRestaurant = (restaurant) => {
+        setSelectedRestaurant(restaurant);
+    };
+
     return (
-        <div>
-        {restaurants.map((restaurant) => (
-            <Restaurant restaurant={restaurant} />
-        ))}
+        <div className={style.root}>
+            {restaurants.map((restaurant) => (
+                <Button className={style.pageLink} onClick={() => selectRestaurant(restaurant)}>
+                    {restaurant.name}
+                </Button>
+            ))}
+
+            {selectedRestaurant && <Restaurant restaurant={selectedRestaurant} />}
         </div>
-    )
-}
+    );
+};
