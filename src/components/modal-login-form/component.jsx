6dcdb87b519b2ae4818ce88b54f1a9229a1c3d5d
impiia <impiia28@gmail.com>
2024-02-02 23:07:43 +0400

    Homework 5: Added a Login button to the header. When the Login button is clicked, a modal window appears containing two fields: Name and Email, and two buttons: OK and Cancel. If the user successfully authorizes, their name and a Logout button will be displayed in the header. The review submission form is now visible only for authorized users. In this form, the Name input field has been replaced with the name of the authorized user.

---
+++ src/components/modal-login-form/component.jsx
@@ -0,0 +1,87 @@
+import { useContext, useReducer } from 'react';
+import styles from './styles.module.scss';
+import { createPortal } from 'react-dom';
+import { Button } from '../button/component';
+import { UserContext } from '../../contexts/user';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change_name':
+      return { ...state, name: action.payload };
+    case 'change_email':
+      return { ...state, email: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const Modal = ({ isOpen, onClose }) => {
+  const { setUser } = useContext(UserContext);
+
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    email: "",
+  });
+  const { name, email } = state;
+
+  const handleOkClick = () => {
+    setUser({ name, email });
+    onClose();
+  };
+
+  const handleCancelClick = () => {
+    onClose();
+  };
+
+  const handleNameChange = (event) => {
+    dispatch({ type: 'change_name', payload: event.target.value });
+  };
+
+  const handleEmailChange = (event) => {
+    dispatch({ type: 'change_email', payload: event.target.value });
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={styles.root}>
+      <form className={styles.modal}>
+        <div className={styles.field}>
+          <label htmlFor="name" >
+            Имя:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              value={name} 
+              placeholder="ваше имя"
+              onChange={handleNameChange} 
+              style={{ marginLeft: "10px" }}
+              />
+          </label>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="email" >
+            e-mail:
+            <input
+              id="email"
+              name="email"
+              type="text"
+              value={email}
+              placeholder="e-mail"
+              onChange={handleEmailChange} 
+              style={{ marginLeft: "10px" }}
+              />
+          </label>
+        </div>
+        <div className={styles.buttons}>
+          <Button className={styles.button} onClick={handleCancelClick}>Отмена</Button>
+          <Button className={styles.button} onClick={handleOkClick}>Ок</Button>
+        </div>
+      </form>
+    </div>,
+    document.body
+  );
+};
